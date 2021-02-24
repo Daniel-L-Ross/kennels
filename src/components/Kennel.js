43414c38a9215f0b4a50e5c4a9dcd8634b02b446
Dan Ross <daniellross@gmail.com>
2021-02-24 13:59:45 -0600

    replaced default app.js with kennel.js

---
+++ src/components/Kennel.js
@@ -0,0 +1,29 @@
+import React from "react"
+import "./Kennel.css"
+
+export const Kennel = () => {
+    const timeElapsed = Date.now()
+    const today = new Date(timeElapsed).toDateString()
+    const kennels = {
+        locations: [
+            {
+                name: "East Nashvilee",
+                adress: "500 Puppy Way"
+            },
+            {}
+        ]
+    }
+
+
+    return (
+        <>
+            <h2>Nashville Kennels</h2>
+            <div>Today is {today}</div>
+            <small>Loving care when you're not there.</small>
+            <address>
+                <div>Visit Us at the Nashville North Location</div>
+                <div>500 Puppy Way</div>
+            </address>
+        </>
+    )
+}
