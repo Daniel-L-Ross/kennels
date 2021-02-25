17e0db7a9f5d65d36d202ecb39fc5bb28df6aad0
Dan Ross <daniellross@gmail.com>
2021-02-25 14:33:08 -0600

    added code so the button increases the displayed count by 1

--- src/components/propsAndSate/PropsAndState.js
+++ src/components/propsAndSate/PropsAndState.js
@@ -1,12 +1,20 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 
-export const PropsAndState = ({yourName}) => {
+export const PropsAndState = ({ yourName }) => {
     let [countClicks, setCountClicks] = useState(0)
 
-    // middle of chapt 3
+    const handleClick = () => {
+        //good practice:
+        //make a copy of state, modifiy it, and then setState to the copy
+        const newCountClicks = ++countClicks
+        setCountClicks(newCountClicks)
+    }
+
     return (
         <>
             <h3>Welcome, {yourName}</h3>
+            <p>{countClicks}</p>
+            <button onClick={(handleClick)}>CLICK ME?</button>
         </>
     )
 }
