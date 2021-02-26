d0573e20d6d6b2b9fd03e25e88f20b1054b4effa
Dan Ross <daniellross@gmail.com>
2021-02-26 12:05:40 -0600

    copied code to createContext per the chapter instructions. added comments for additional understanding

---
+++ src/components/animal/AnimalProvider.js
@@ -0,0 +1,52 @@
+import React, { useState, createContext } from "React"
+
+
+/* 
+    The context is imported and used by individual components that need data
+    Whenver a data provider componenet is created in react,
+    a context must be created as well.
+*/
+export const AnimalContext = createContext()
+
+// This component establishes what data can be used.
+export const AnimalProvider = (props) => {
+    // array destructuring to get our initial value and funcion
+    const [animals, setAnimals] = useState([])
+
+    // getAnimals is a what we have been using, except we set the animals
+    // data state to the return value
+    const getAnimals = () => {
+        return fetch("http://localhost:8088/animals?_expand=location")
+            .then(res => res.json())
+            .then(setAnimals)
+    }
+
+    // post method to add an animal
+    const addAnimal = animalObj => {
+        return fetch("http://localhost:8088/animals", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(animalObj)
+        })
+            .then(getAnimals)
+    }
+
+    /*
+        You return a context provider which has the
+        `animals` state, `getAnimals` function,
+        and the `addAnimal` function as keys. This
+        allows any child elements to access them.
+    */
+    return (
+        // this is where the context is sent out to be used
+        // by child compenents. the VALUE attribute holds
+        // the data we are sending here
+        <AnimalContext.Provider value={{
+            animals, getAnimals, addAnimal
+        }}>
+            {props.children}
+        </AnimalContext.Provider>
+    )
+}
