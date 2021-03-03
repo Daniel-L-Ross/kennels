1ce73842fdc65f13347c8b2d806bc8f5c92d42b0
Dan Ross <daniellross@gmail.com>
2021-03-03 15:51:16 -0600

    Added button to employee card for remove function. Waiting to add more code until employee card renders on a unique page

--- src/components/employee/EmployeeDetail.js
+++ src/components/employee/EmployeeDetail.js
@@ -1,8 +1,20 @@
-import React from "react"
+import React, { useContext, useEffect, useState } from "react"
+import { useParams, useHistory } from  "react-router-dom"
+import { EmployeeContext } from "./EmployeeProvider"
+import "./Employee.css"
 
-export const EmployeeCard = ({employee}) => (
-<section className="employee">
-    <h3 className="employee__name">{employee.name}</h3>
-    <div className="employee__location">Location: {employee.location.name}</div>
-</section>
-)
+export const EmployeeCard = ({ employee }) => {
+    const history = useHistory()
+    
+    const handleRemove = () => {
+        history.push("/employees")
+    }
+
+    return (
+        <section className="employee">
+            <h3 className="employee__name">{employee.name}</h3>
+            <div className="employee__location">Location: {employee.location.name}</div>
+            <button onClick={handleRemove}>Remove Employee</button>
+        </section>
+    )
+}
